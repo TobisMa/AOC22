84dc8e4300f4f594df45cd997518d5fed8c2b52c
TobisMa <tobias-ma@gmx.net>
2022-12-04 06:27:08 +0100

    Day4 sets are cool

---
+++ d04/part2.py
@@ -0,0 +1,22 @@
+from util import get_data
+import builtins
+
+def range(x, y=None, z=None):
+    if y is None and z is None:
+        return builtins.range(x)
+    elif z is None:
+        return builtins.range(x, y + 1)
+    return builtins.range(x, y + 1, z)
+
+data = get_data(("\n", ","))
+# data = [
+#     ["2-4", "2-8"]
+# ]
+data = [[set(range(*map(int, x.split("-")))) for x in d] for d in data]
+
+same = 0
+for d in data:
+    result = d[0] | d[1]
+    if len(result) != len(d[0]) + len(d[1]):
+        same += 1
+print(same)
