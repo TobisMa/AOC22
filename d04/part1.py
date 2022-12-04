c811e0d2f69fcf56230ca109c03122c5dfc0c968
TobisMa <tobias-ma@gmx.net>
2022-12-04 11:37:04 +0100

    Day4 Added typing

--- d04/part1.py
+++ d04/part1.py
@@ -1,22 +1,23 @@
+from typing import List, Optional, Set
 from util import get_data
 import builtins
 
-def range(x, y=None, z=None):
+def range(x: int, y: Optional[int]=None, z: Optional[int]=None, /) -> builtins.range:
     if y is None and z is None:
         return builtins.range(x)
     elif z is None:
         return builtins.range(x, y + 1)
     return builtins.range(x, y + 1, z)
 
-data = get_data(("\n", ","))
+data: List[List[str]] = get_data(("\n", ","))
 # data = [
 #     ["2-4", "2-8"]
 # ]
-data = [[set(range(*map(int, x.split("-")))) for x in d] for d in data]
+data: List[List[Set[int]]] = [[set(range(*map(int, x.split("-")))) for x in d] for d in data]
 
-same = 0
+same: int = 0
 for d in data:
-    result = d[0] & d[1]
+    result: Set[int] = d[0] & d[1]
     if result in d:
         same += 1
 print(same)
