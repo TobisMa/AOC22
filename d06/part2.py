4216d7d221fe538a861b601670814eb23253dcbd
TobisMa <tobias-ma@gmx.net>
2022-12-07 07:39:39 +0100

    removed unnecessry 0

--- d06/part2.py
+++ d06/part2.py
@@ -8,7 +8,7 @@ print(d)
 m = 0
 for data in d:
     if not data: continue
-    for i in range(0, len(data[:-14])):
+    for i in range(len(data[:-14])):
         if len(set(data[i:i+14])) == 14:
             m += i + 14
             break
