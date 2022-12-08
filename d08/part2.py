2bc1d1cdafc4bcdc0b552fa155bf31ce4801d336
TobisMa <tobias-ma@gmx.net>
2022-12-08 07:59:47 +0100

    Day8 removed visibility calculation

--- d08/part2.py
+++ d08/part2.py
@@ -24,26 +24,18 @@ def get_neightbours(x: int, y: int):
         except IndexError:
             yield -1
 
-visible = set()
 tree_dic = {}
 for y, trees in enumerate(data):
     # visible.append([])
     for x, tree in enumerate(trees):
         scores = []
         for delta in range(-1, 2, 2):
-            scores.append(0)
-            if x == 0 or y == 0 or x == len(trees) - 1 or y == len(data) - 1:
-                visible.add((x, y))
-                continue
-            
+            scores.append(0)            
             stop = 0 if delta < 0 else len(trees)
             for delta_x in range(x + delta, stop + min(0, delta), delta):
                 scores[-1] += 1
                 if data[y][delta_x] >= tree:
                     break
-            else:
-                if len(range(x + delta, stop + min(0, delta), delta)):
-                    visible.add((x, y))
             
             scores.append(0)
                 
@@ -52,9 +44,6 @@ for y, trees in enumerate(data):
                 scores[-1] += 1
                 if data[delta_y][x] >= tree:
                     break
-            else:
-                if len(range(y + delta, stop + min(0, delta), delta)):
-                    visible.add((x, y))
                     
         if x == 0 or y == 0 or x == len(trees) - 1 or y == len(data) - 1:
             tree_dic[(x, y)] = 0
