f4ae7ca013ad76ddce9a7ccb88da3759745b5ee8
TobisMa <tobias-ma@gmx.net>
2022-12-10 23:21:26 +0100

    Day09 part2 wtf i dont have a clue

--- d09/part2.py
+++ d09/part2.py
@@ -13,14 +13,6 @@ R 17
 D 10
 L 25
 U 20""".split("\n")
-# d = """R 4
-# U 4
-# L 3
-# D 1
-# R 4
-# D 1
-# L 5
-# R 2""".split("\n")
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -51,22 +43,56 @@ def sign(n: int) -> Literal[-1, 0, 1]:
         return -1
     return 1 if n != 0 else 0
 
+color_cycle = [
+    [255, 0, 0],
+    [255, 255, 0],
+    [255, 0, 255],
+    [255, 255, 255],
+    [0, 255, 0],
+    [0, 255, 255],
+    [0, 0, 255],
+    [200, 200, 200],
+    [100, 0, 100],
+    [100, 100, 0]
+]
+
 def draw():
     delta = [400, 300]
     screen.fill([0]*3)
-    for knot in knots:
-        pos = knot
+    for i, knot in enumerate(knots):
+        pos = knot.copy()
         pos[1] *= -1
         pos = add(delta, mul(10, pos))
-        pygame.draw.circle(screen, [255, 0, 0], pos, 5)
+        pygame.draw.circle(screen, color_cycle[i % len(color_cycle)], pos, 5)
+
+    for i in range(5, 800, 10):
+        pygame.draw.line(screen, [255]*3, [i, 0], [i, 600])
+
+    for i in range(5, 600, 10):
+        pygame.draw.line(screen, [255]*3, [0, i], [800, i])
 
     tc = time()
     te = tc + 0.1
     while tc < te:
-        for _ in pygame.event.get():
-            ...
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(-1)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    break_ = False
+                    while True:
+                        for event in pygame.event.get():
+                            if event.type == pygame.QUIT:
+                                pygame.quit()
+                                exit(-1)
+                            elif event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_SPACE:
+                                    break_=True
+                        if break_:
+                            break
         tc = time()
-        pygame.display.flip()
+    pygame.display.flip()
         
 
 
@@ -87,7 +113,7 @@ def adjacent(vec1, vec2):
     return distance(vec1, vec2) <=  sqrt(2) + 0.1
 
 def diagonally_adjacent(vec1, vec2):
-    return distance(vec1, vec2) >= sqrt(2) - 0.0001
+    return distance(vec1, vec2) >= sqrt(2) - 0.3
 
 positions = []
 diagonal = False
@@ -96,27 +122,24 @@ last_direction = None
 for step, (direction, count) in enumerate(data):
     for _ in range(count):
         knots[0] = add(knots[0], moves[direction])
-        
         for j in range(len(knots) - 1):
+            if adjacent(knots[j], knots[j+1]): 
+                continue
             
-            if not adjacent(knots[j], knots[j + 1]):
-                knots[j + 1] = add(knots[j + 1], moves[direction])
+            knots[j+1] = add(knots[j+1], moves[direction])
+            if diagonally_adjacent(knots[j], knots[j+1]) and last_direction != direction:
+                if abs(knots[j][0] - knots[j+1][0]) >= 1 and direction in ("U", "D"):
+                    knots[j+1][0] += sign(knots[j][0] - knots[j+1][0])
+                elif abs(knots[j][1] - knots[j+1][1]) >= 1 and direction in ("L", "R"):
+                    knots[j+1][1] += sign(knots[j][1] - knots[j+1][1])
                 
-                if diagonally_adjacent(knots[j], knots[j + 1]) and last_direction != direction:
-                    if abs(knots[j][0] - knots[j + 1][0]) >= 1 and direction in ("U", "D"):
-                        knots[j + 1][0] += sign(knots[j][0] - knots[j + 1][0])
-                        
-                    elif abs(knots[j][1] - knots[j + 1][1]) >= 1 and direction in ("L", "R"):
-                        knots[j + 1][1] += sign(knots[j][1] - knots[j + 1][1])
-                    
-            draw()
+        draw()
         positions.append(tuple(knots[-1]))
-    # input()
     print(f"{knots=}; {direction=}, {count=}; {step=}")
     # input()
     last_direction = direction
 
-print(positions)
+# print(positions)
 print(len(set(positions)))
 pygame.quit()
         
